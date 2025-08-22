40bccf4224d68b69a275cb357375f3d30a8886e8
Victoria Reyes <victoriavalencia2006@outlook.com>
2025-08-21 21:51:26 -0600

    Estructura del proyecto

---
+++ src/main/java/com/esfe/sistemaDeCitas/SistemaDeCitasApplication.java
@@ -0,0 +1,13 @@
+package com.esfe.sistemaDeCitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeCitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeCitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esfe/sistemaDeCitas/SistemaDeCitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esfe.sistemaDeCitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeCitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
